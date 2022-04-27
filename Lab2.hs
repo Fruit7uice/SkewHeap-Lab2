d67f264b0e43d7da5787d8a7d5c0d38ae2eb0691
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-04-27 10:56:33 +0200

    Orderbook and trade

--- Lab2.hs
+++ Lab2.hs
@@ -1,7 +1,7 @@
 import Control.Applicative
 import System.Environment
 import System.IO
-
+import SKEWHEAP
 -- | Bids.
 
 data Bid
@@ -13,6 +13,33 @@ data Bid
 
 type Person = String
 type Price = Integer
+type BuyBid = SkewHeap Bid
+type SellBid = SkewHeap Bid
+
+data Orderbook 
+  = Queues BuyBid SellBid 
+    deriving (Show)
+
+instance Eq BuyBid where
+    Empty == Empty = 0 == 0
+    Empty == _ = 0 == 1
+    _ == Empty = 1 == 0
+    (Node (Buy _ x) _ _) == (Node(Buy _ y) _ _) =  x == y
+    (Node (Buy _ x) _ _) == (Node(Sell _ y) _ _) =  x == y
+
+
+instance Ord BuyBid where
+    Empty `compare` Empty = 0 `compare` 0
+    Empty `compare` _ = 0 `compare` 1
+    _ `compare` Empty = 1 `compare` 0
+    (Node (Buy _ x) _ _) `compare` (Node(Buy _ y) _ _) =  x `compare` y
+    (Node (Buy _ x) _ _) `compare` (Node(Sell _ y) _ _) =  x `compare` y
+    (Node (Sell _ x) _ _) `compare` (Node(Sell _ y) _ _) =  x `compare` y
+
+sellfoo = Node(Sell "Adam" 40) Empty Empty
+buyfoo = Node(Buy "Jonte"  30) Empty Empty
+
+orderTest = Queues (Node (Buy "Adam" 40) Empty Empty) (Node (Sell "Jonte" 30) Empty Empty) 
 
 -- | Parses a bid. Incorrectly formatted bids are returned verbatim
 -- (tagged with 'Left').
@@ -61,6 +88,39 @@ main = do
   process h = trade =<< parseBids =<< hGetContents h
 
 -- | The core of the program. Takes a list of bids and executes them.
+orderBook = Queues Empty Empty
 
-trade :: [Bid] -> IO ()
-trade = undefined
+trade :: [Bid] -> IO()
+trade xs = trade' orderBook xs
+
+trade' :: OrderBook -> [Bid] -> IO()
+trade' ob@(Queues bb sb) (x:xs) = do
+   case x of
+    (Node (Buy _ _) _ _) -> buyBidComp x ob
+  --   (Node (Sell _ _) _ _) -> addToSkew x sb
+  --   (Node (NewBuy _ _ _) _ _) -> newBuy
+  --   (Node (NewSell _ _ _) _ _) -> newSell
+
+  -- compare bids bb sb
+
+buyBidComp :: Bid -> OrderBook -> IO()
+buyBidComp x@(Node(Buy n p)) ob@(Queues bb sb) = do
+    y(Node (Sell n2 _) _ _) <- getmin sb
+    if x >= y
+      then 
+        deleteMin sb
+        printStrLn(n ++ " buys a share from " ++ show n2 ++ " for " ++ p ++ "kr")
+      else
+        add x bb
+  
+
+
+
+
+{-
+isEmpty :: [a] -> Bool
+isEmpty = \myList ->
+  case myList of
+    [] -> True -- if the list is empty, return true
+    _ -> False -- otherwise, return false
+-}
--- skewHeap.hs
+++ skewHeap.hs
@@ -1,22 +1,32 @@
-module SKEWHEAP() where
+module SKEWHEAP where
 
 import Prelude hiding (lookup)
 
 data SkewHeap a = 
-    Empty | Node a (SkewHeap a) (SkewHeap a) deriving(Show, Eq)
+    Empty | Node a (SkewHeap a) (SkewHeap a) deriving(Show)
 
 
 testHeap = Node 3 (Node 4 (Node 6 Empty Empty) Empty) (Node 5 (Node 7 Empty Empty) Empty)
 
+
+
 singleton :: a -> SkewHeap a 
 singleton x = Node x Empty Empty  
 
-merge :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
-merge t1 Empty = t1
-merge Empty t2 = t2
-merge t1@(Node x1 left1 right1) t2@(Node x2 left2 right2)
-   | x1 <= x2    = Node x1 (merge t2 right1) left1
-   | otherwise   = Node x2 (merge t1 right2) left2
+mergeMin :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
+mergeMin t1 Empty = t1
+mergeMin Empty t2 = t2
+mergeMin t1@(Node x1 left1 right1) t2@(Node x2 left2 right2)
+   | x1 <= x2    = Node x1 (mergeMin t2 right1) left1
+   | otherwise   = Node x2 (mergeMin t1 right2) left2
+
+mergeMax :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
+mergeMax t1 Empty = t1
+mergeMax Empty t2 = t2
+mergeMax t1@(Node x1 left1 right1) t2@(Node x2 left2 right2)
+   | x1 <= x2    = Node x1 (mergeMax t2 right1) left1
+   | otherwise   = Node x2 (mergeMax t1 right2) left2
+
 
 
 lookup :: Ord a => a -> SkewHeap a -> Maybe a
@@ -28,16 +38,20 @@ lookup x (Node y r l)
 
 
 add :: Ord a => a -> SkewHeap a -> SkewHeap a
-add x t = merge (singleton x) t
+add x t = mergeMin (singleton x) t
 
 
 deleteMin :: Ord a => SkewHeap a -> SkewHeap a
-deleteMin (Node y l r) = merge l r
+deleteMin (Node y l r) = mergeMin l r
 
 
 delete :: Ord a => a -> SkewHeap a -> SkewHeap a
 delete x t@(Node y l r)
-    | x == y = merge l r
+    | x == y = mergeMin l r
     | lookup x l == Just x = (Node y (delete x l) r)
     | lookup x r == Just x = (Node y l (delete x r))
     | otherwise = t
+
+getMin :: Ord a => SkewHeap a -> Maybe (a)
+getMin Empty        = Nothing
+getMin (Node x _ _) = Just (x)
