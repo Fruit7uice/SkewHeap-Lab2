5531fe1dae0b682dd637350963d0445d4321fa50
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-04-28 18:45:20 +0200

    Time-complexity comments
    Time-complexity comments

--- skewHeap.hs
+++ skewHeap.hs
@@ -7,12 +7,15 @@ data SkewHeap a =
     Empty | Node a (SkewHeap a) (SkewHeap a) deriving(Show)
 
 -- Heap that only consist of one single node and no children.
+-- The time-complexity for this function is O(1).
 
 singleton :: a -> SkewHeap a 
 singleton x = Node x Empty Empty  
 
+
 -- Merge-function that merges two heaps together depending on
 -- the value of the roots of the merging heaps.
+-- The time-complexity for the mergeMin-function is amortized O(log n).
 
 mergeMin :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
 mergeMin t1 Empty = t1
@@ -23,6 +26,7 @@ mergeMin t1@(Node x1 left1 right1) t2@(Node x2 left2 right2)
 
 -- Lookup function, given a key to search for the function will 
 -- retrieve the key if it exists. 
+-- The time-complexity for the lookup-function is O(n).
 
 lookup :: Ord a => a -> SkewHeap a -> Maybe a
 lookup _ Empty = Nothing
@@ -32,16 +36,19 @@ lookup x (Node y r l)
     | x > y = lookup x r
 
 -- Function that adds a single node to an existing heap.
+-- The time-complexity for the addNode-function is amortized O(log n).
 
 addNode :: Ord a => a -> SkewHeap a -> SkewHeap a
 addNode x t = mergeMin (singleton x) t
 
 -- Function that deletes the root of a heap.
+-- The time-complexity for the addNode-function is amortized O(log n).
 
 deleteRoot :: Ord a => SkewHeap a -> SkewHeap a
 deleteRoot (Node y l r) = mergeMin l r
 
 -- Function that deletese any given node of a heap.
+-- The time-complexity for the delete-function is amortized O(log n).
 
 delete :: Ord a => a -> SkewHeap a -> SkewHeap a
 delete x t@(Node y l r)
@@ -51,6 +58,7 @@ delete x t@(Node y l r)
     | otherwise = t
 
 -- Function that returns the root of a given heap.
+-- The time-complexity for the getRoot-function is O(1).
 
 getRoot :: Ord a => SkewHeap a -> Maybe(a)
 getRoot Empty = Nothing
